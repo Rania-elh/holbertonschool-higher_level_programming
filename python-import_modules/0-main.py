ddffaa693be365ed48bb0dfc5b5294af302f9628
Rania-elh <rania1elhefnawy@gmail.com>
2025-01-17 22:56:01 +0100

    Update files

---
+++ python-import_modules/0-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value is the sum of a and b
+    """
+    # La fonction prend deux arguments, a et b, qui sont des entiers.
+    # Elle retourne la somme de ces deux entiers.
+
+    return (a + b)
+    # La fonction retourne le résultat de l'addition de a et b.
+
