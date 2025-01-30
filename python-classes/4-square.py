41b9ca5d1a950a10b9ffdf659bb75b3320f37308
Rania-elh <rania1elhefnawy@gmail.com>
2025-01-30 15:06:31 +0100

    python-classes

---
+++ python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+class Square:
+    """Represents a square"""
+    def __init__(self, size=0)
+        """Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
+
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
